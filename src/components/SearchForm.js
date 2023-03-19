8cec7945592d5779cf5566223876223e95ba9d84
MattMcKay <matthewmckay@gmail.com>
2023-03-19 14:41:12 -0400

    trying to get some art work on the page

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,11 +1,71 @@
 import React from 'react'
+import { useState } from 'react'
 
 const SearchForm = () => {
+
+    const [artSearch, setArtSearch] = useState([]);
+    const [userInput, setUserInput] = useState('');
+    const [searchError, setSearchError] = useState(false);
+
+    const searchArt = async (e) => {
+        const url = new URL (`https://www.rijksmuseum.nl/api/en/collection`);
+
+        e.preventDefault();
+        url.search = new URLSearchParams({
+            key: `C1So9sXo`,
+            format: 'json',
+            imgonly: true,
+            q: userInput,
+        });
+
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log(data);
+            setArtSearch(data.artObjects);
+            if (data.artObjects.length === 0) {
+                throw new Error()
+            } else {
+                setSearchError(false)
+            }
+        }
+        catch (error){
+            setSearchError(true);
+        }
+    }
+
+    const handleChange = (e) => {
+        setUserInput(e.target.value);
+    }
+
   return (
-    <div>
-      <h2>poop</h2>
-    </div>
+    <>
+        <div>
+            <form onSubmit={searchArt}>
+                <label className="labelSearch" htmlFor="userSearch">Search for beautiful art from the Rijks Museum: </label>
+                <p>Type into the search field below and then click the 'Search' button!</p>
+                <input
+                    onChange={handleChange}
+                    type="text"
+                    id="userSearch"
+                    value={userInput}
+                    placeholder={`e.g. "picasso"`}
+                    className="input"
+                    required
+                />
+                <button type="submit" className="button">Search</button>
+                {searchError ? <p className="errorMessage">Sorry, your search didn't return any of our timeless works of art. Please try a different search.</p> : null}
+            </form>
+        </div>
+        <div className='imageFlex'>
+            {artSearch.map((artwork) => {
+                return (
+                    
+                )
+            })}
+        </div>
+    </>
   )
 }
 
-export default SearchForm
+export default SearchForm;
