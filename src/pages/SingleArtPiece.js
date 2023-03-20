79be3433d08dee25cd5d8ed7fc0fe46856ccc028
MattMcKay <matthewmckay@gmail.com>
2023-03-20 10:49:19 -0400

    Getting some stuff on the SingleArtPiece page

--- src/pages/SingleArtPiece.js
+++ src/pages/SingleArtPiece.js
@@ -3,30 +3,52 @@ import Loading from '../components/Loading';
 import { useParams, Link } from 'react-router-dom';
 import { useState } from 'react';
 
+
 const SingleArtPiece = () => {
     const { id } = useParams();
+    const [loading, setLoading] = useState(false);
     const [artPiece, setArtPiece] = useState(null);
     
-    const artInfo = async () => {
-        const url = new URL(`https://www.rijksmuseum.nl/api/en/collection/${id}`);
-        url.search = new URLSearchParams({
-            key: `C1So9sXo`,
-            format: "json",
-            imgonly: true,
-        });
-        const res = await fetch(url);
-        const data = await res.json();
-        console.log(data)
-        // if (data.artObject) {
-        //    setArtPiece(data.artObject)
-        // } else {
-        //     setArtPiece(null)
-        // }
+    React.useEffect(() => {
+        setLoading(true);
+        const artInfo = async () => {
+            try {
+                const url = new URL(`https://www.rijksmuseum.nl/api/en/collection/${id}`);
+                url.search = new URLSearchParams({
+                    key: `C1So9sXo`,
+                    format: "json",
+                    imgonly: true,
+                });
+                const res = await fetch(url);
+                const data = await res.json();
+                console.log(data.artObject)
+                if (data.artObject) {
+                    setArtPiece(data.artObject)
+                } else {
+                    setArtPiece(null)
+                }
+                setLoading(false)
+            } catch (error) {
+                console.log(error)
+                setLoading(false)
+            }
+            }
+        artInfo();
+    },[id])
+
+    if (loading) {
+        return <Loading />
     }
-    artInfo();
+    if (!artPiece) {
+        return <h2>No priceless piece of art to display</h2>
+    }
+   console.log(artPiece.materials)
     return (
         <section className='section artpiece-section'>
-            <h2></h2>
+            <article>
+                 <p>{artPiece.materials}</p>
+                 <p>{JSON.stringify(artPiece.acquisition)}</p>
+            </article>
     </section>
   )
 }
