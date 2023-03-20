0d1abb03c27f706286658db0bf5c810213dffa8d
MattMcKay <matthewmckay@gmail.com>
2023-03-19 22:02:40 -0400

    done for the night. making progress on single art piece info

--- src/pages/SingleArtPiece.js
+++ src/pages/SingleArtPiece.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Loading from '../components/Loading';
 import { useParams, Link } from 'react-router-dom';
-
+import { useState } from 'react';
 
 const SingleArtPiece = () => {
     const { id } = useParams();
+    const [artPiece, setArtPiece] = useState(null);
     
     const artInfo = async () => {
         const url = new URL(`https://www.rijksmuseum.nl/api/en/collection/${id}`);
@@ -15,14 +16,18 @@ const SingleArtPiece = () => {
         });
         const res = await fetch(url);
         const data = await res.json();
-        console.log(data);
+        console.log(data)
+        // if (data.artObject) {
+        //    setArtPiece(data.artObject)
+        // } else {
+        //     setArtPiece(null)
+        // }
     }
-
     artInfo();
     return (
-        <div>
-      poops
-    </div>
+        <section className='section artpiece-section'>
+            <h2></h2>
+    </section>
   )
 }
 
