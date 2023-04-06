52a6baa272279e56e019b6aa163a1e4218886ec5
MattMcKay <matthewmckay@gmail.com>
2023-04-06 14:58:24 -0400

    Updated paragraph in about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <section className='section about-section'>
             <h1 className='section-title'>about me</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus optio ipsam eos perferendis quam quasi? Deserunt sit illum assumenda rem tempore, maiores quo dolorem officia magni earum, voluptatum eius harum. Cum excepturi enim officiis amet nesciunt velit minima saepe iste?</p>
+            <p>I am a software developer with experience building and testing software systems. I have a proven ability to deliver high quality software solutions to a variety of business and technical needs and a proven track record of cultivating strong relationships with all team members and customers alike. I understand that success is about delivering timely solutions to customers that are high quality, both external (what the customer sees) and internal (what the developer sees and maintains). I value building a partnership with the customers and working closely with them to understand their needs so that we solve the right problem the right way. I seek to understand the full depth and multi-dimensional aspects of problems. I enjoy complex assignments that tax my intellect, talents and skills to the limit. I have an excellent ability to break ideas down systematically. I am devoted to rationality, logic and rigorous analysis and apply it to nearly all problems I encounter. I relish opportunities to influence others, inspire enthusiasm, improve morale and hold others capable.</p>
         </section>
     )
 }
