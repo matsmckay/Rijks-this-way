85b7aa1d2f1038d6eeb53f996f20c162af99fc48
MattMcKay <matthewmckay@gmail.com>
2023-06-29 10:00:19 -0400

    Changed loading component from displaying simple text on a load to an alternating three bouncy ball animation. Looks pretty darn cool

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const Loading = () => {
- return (
-  <div className="loader">
-    Loading...
-  </div>
- )
+  return (
+    <div className='loader'>
+      <span className='loader__ball'></span>
+      <span className='loader__ball'></span>
+      <span className='loader__ball'></span>
+    </div>
+  )
 }
 
 export default Loading
